9dab09dc62b3276c631dd5df3ed38a2e8824e462
Alberto Hamilton <albham@ull.edu.es>
2022-03-28 19:15:14 +0100

    Colocar boton de edición en cabecera página

---
+++ Boton edicion arriba/Boton edicion arriba.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name     Boton edicion arriba
+// @version  1
+// @include  https://campus*.ull.es/course/view.php*
+// @description   Autor: Alberto Hamilton 2020. Licencia: GPLv3.
+// @grant    none
+// ==/UserScript==
+
+
+function colocaBotonEdicion() {
+  const buttons = document.getElementsByTagName('button');
+  
+  console.log('Encontrados ' + buttons.length + ' botones en página');
+
+  for(let i = 0; i < buttons.length; i++) {
+    const ba = buttons[i];
+		if(ba.innerText.match(/ctivar edición/)) {
+    	console.log(`Encontrado boton con texto "${ba.innerText}"`);
+      ba.style.cssText += 'position:fixed;top:0px;left:200px;z-index:1000;border-color: white;';
+    }
+  }
+}
+
+//setTimeout(colocaBotonEdicion, 10000);
+
+
+colocaBotonEdicion();
